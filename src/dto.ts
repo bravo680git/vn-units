e3a6ab4bb047190195474c6a233b5c1e934e64fd
bravo <iambravo680@gmail.com>
2025-03-18 12:43:02 +0700

    chore: return code as string instead of number

--- src/dto.ts
+++ src/dto.ts
@@ -19,40 +19,40 @@ type WardRaw = DistrictRaw & {
 export class ProvinceDto {
   protected type: string;
   protected typeVi: string;
-  protected provinceCode: number;
+  protected provinceCode: string;
   protected provinceName: string;
   protected provinceNameVi: string;
 
   constructor(raw: ProvinceRaw, normalizeVietnamese?: boolean) {
     this.type = getUnitType(raw.LoaiHinh);
     this.typeVi = raw.LoaiHinh;
-    this.provinceCode = parseInt(raw.MaTinh);
+    this.provinceCode = raw.MaTinh;
     this.provinceName = formatUnitName(raw.TenTinh, normalizeVietnamese);
     this.provinceNameVi = raw.TenTinh;
   }
 }
 
 export class DistrictDto extends ProvinceDto {
-  protected districtCode: number;
+  protected districtCode: string;
   protected districtName: string;
   protected districtNameVi: string;
 
   constructor(raw: DistrictRaw, normalizeVietnamese?: boolean) {
     super(raw, normalizeVietnamese);
-    this.districtCode = parseInt(raw.MaQuanHuyen);
+    this.districtCode = raw.MaQuanHuyen;
     this.districtName = formatUnitName(raw.TenQuanHuyen, normalizeVietnamese);
     this.districtNameVi = raw.TenQuanHuyen;
   }
 }
 
 export class WardDto extends DistrictDto {
-  protected wardCode: number;
+  protected wardCode: string;
   protected wardName: string;
   protected wardNameVi: string;
 
   constructor(raw: WardRaw, normalizeVietnamese?: boolean) {
     super(raw, normalizeVietnamese);
-    this.wardCode = parseInt(raw.MaPhuongXa);
+    this.wardCode = raw.MaPhuongXa;
     this.wardName = formatUnitName(raw.TenPhuongXa, normalizeVietnamese);
     this.wardNameVi = raw.TenPhuongXa;
   }
